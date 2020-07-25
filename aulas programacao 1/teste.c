2cc6b1ecce1c235607c5cf38d306a38d2a8b654b
Thayanna <thayannaklysnney@gmail.com>
2020-07-24 21:34:35 -0300

    Add files via upload

---
+++ aulas programacao 1/teste.c
@@ -0,0 +1,25 @@
+//Linguagem C
+
+//bibliotecas
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int main(){
+	
+	int result;
+	int x = 5;
+	char c = 'd';
+
+    //result = 5+5;
+	result = x+x;
+
+
+	printf("%i + %i = %i",x,x,result);
+	printf("Letra = %c",c);
+	printf("numero = %i\n",x);
+
+
+
+	return 0;
+}
